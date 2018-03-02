c0553076dde59355d3910259f0950cbcc7d9c352
lacecistat <cecilia.balocchi@gmail.com>
2018-03-02 10:42:20 -0500

    Update various_functions.h
    added which_is_nearest_k

--- src/various_functions.h
+++ src/various_functions.h
@@ -13,6 +13,7 @@ arma::mat Submatrix(arma::mat M, int n_rows, int n_cols, int* row_index, int* co
 void Connected_Components(arma::mat M, int n, int* components, int* count);
 void DFSUtil(arma::mat M, int n, int v, bool* visited, int* components, int* count);
 int* which_is_nearest(arma::mat centroids, arma::mat data);
+int* which_is_nearest_k(arma::mat centroids, arma::mat data);
 arma::mat Distance_matrix(double* beta_hat, int nObs);
 
 #endif /* VARIOUS_FUNCTIONS_H_ */
